ef1c78b5ecdd12b053bd4a71922bcbc9b3d607b7
Subhajit Sahu <wolfram77@gmail.com>
2020-10-13 10:38:18 +0530

    :octopus: wrote skeleton tiled kernel

--- main.cu
+++ main.cu
@@ -41,6 +41,26 @@ __global__ void kernel_simple(float *a, float *x, float *y, int xr, int xc, int 
 }
 
 
+__global__ void kernel_tiled(float *a, float *x, float *y, int xr, int xc, int yc) {
+  __shared__ float as[BY*BX];
+  __shared__ float xs[BY*BX];
+  __shared__ float ys[BY*BX];
+  int r = by*BY + ty;
+  int c = bx*BX + tx;
+  as[ty][tx] = 0;
+  for (int i=0; i<xc; i+=BX) {
+    __syncthreads();
+    xs[ty][tx] = x[by*BY + ty][bx*BX + i+tx];
+    ys[ty][tx] = y[by*BY + i+ty][bx*BX + tx];
+    __syncthreads();
+    for (int j=0; j<BX; j++)
+      as[ty][tx] += xs[ty][j] * ys[j][tx];
+  }
+  __syncthreads();
+  a[by*BY + ty][bx*BX + tx] = as[ty][tx];
+}
+
+
 float test_simple(float *a, float *x, float *y, int xr, int xc, int yc) {
   int A1 = xr * yc * sizeof(float);
   int X1 = xr * xc * sizeof(float);
@@ -76,6 +96,45 @@ float test_simple(float *a, float *x, float *y, int xr, int xc, int yc) {
   TRY( cudaFree(aD) );
   return duration;
 }
+
+
+
+float test_tiled(float *a, float *x, float *y, int xr, int xc, int yc) {
+  int A1 = xr * yc * sizeof(float);
+  int X1 = xr * xc * sizeof(float);
+  int Y1 = xc * yc * sizeof(float);
+
+  cudaEvent_t start, stop;
+  TRY( cudaEventCreate(&start) );
+  TRY( cudaEventCreate(&stop) );
+
+  void *aD, *xD, *yD;
+  TRY( cudaMalloc(&aD, A1) );
+  TRY( cudaMalloc(&xD, X1) );
+  TRY( cudaMalloc(&yD, Y1) );
+
+  TRY( cudaMemcpy(xD, x, X1, cudaMemcpyHostToDevice) );
+  TRY( cudaMemcpy(yD, y, Y1, cudaMemcpyHostToDevice) );
+
+  dim3 threads(16, 16);
+  dim3 blocks(CEILDIV(xr, 16), CEILDIV(yc, 16));
+  kernel_tiled<<<blocks, threads>>>(aD, xD, yD, xr, xc, yc);
+
+  TRY( cudaMemcpy(a, aD, A1, cudaMemcpyDeviceToHost) );
+
+  float duration;
+  TRY( cudaEventRecord(stop, 0) );
+  TRY( cudaEventSynchronize(stop); );
+  TRY( cudaEventElapsedTime(&duration, start, stop) );
+
+  TRY( cudaEventDestroy(start) );
+  TRY( cudaEventDestroy(stop) );
+  TRY( cudaFree(yD) );
+  TRY( cudaFree(xD) );
+  TRY( cudaFree(aD) );
+  return duration;
+}
+
 
 
 int main() {
