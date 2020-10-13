8d99e21062bf00c7fd0b1f018da6013ce2c602c8
Subhajit Sahu <wolfram77@gmail.com>
2020-10-13 15:09:47 +0530

    smaller numbers

--- main.h
+++ main.h
@@ -20,11 +20,11 @@ void matrix_product(float* a, float* x, float* y, int XR, int XC, int YC) {
 void test_populate(float *x, float *y, int XR, int XC, int YC) {
   for (int r=0; r<XR; r++) {
     for (int c=0; c<XC; c++)
-      GET2D(x, r, c, XC) = (float) r*XC + c;
+      GET2D(x, r, c, XC) = (r*XC + c)*1e-8f;
   }
   for (int r=0; r<XC; r++) {
     for (int c = 0; c < YC; c++)
-      GET2D(y, r, c, YC) = (float) r*YC + c;
+      GET2D(y, r, c, YC) = (r*YC + c)*1e-8f;
   }
 }
 
@@ -34,7 +34,7 @@ void test_print(float *exp, float *ans, int R, int C, float duration) {
   printf("Matrix element sum: %.5g\n", SUM_ARRAY(ans, R*C));
   if (exp) {
     int cmp = memcmp(exp, ans, R*C * sizeof(float));
-    if (cmp != 0) printf("Result is invalid!\n");
+    if (cmp != 0) printf("Result doesnt match!\n");
   }
   printf("\n");
 }
